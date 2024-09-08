d6c0a77fef319803d9fda4ceb8d8c3418f95b344
Daeyoung Kim <dkim@cellinobio.com>
2024-09-08 14:51:17 -0400

    Add image histogram to the output

--- assignments/01-historgrams-binary-images/main.py
+++ assignments/01-historgrams-binary-images/main.py
@@ -8,9 +8,14 @@ def main() -> None:
     img = cv.imread(img_path + image_name, cv.IMREAD_GRAYSCALE)
 
     plt.hist(img.ravel(), 256, (0, 256))
+    plt.xlabel("Grayscale value")
+    plt.ylabel("Count")
+    plt.savefig(
+        f'results/{image_name.split(".")[0]}_histogram.png', bbox_inches="tight"
+    )
     plt.show()
 
-    threshold = 100
+    threshold = 150
     binary_img = cv.threshold(img, threshold, 255, cv.THRESH_BINARY)[1]
     cv.imwrite(f"results/{image_name.split(".")[0]}_binary.png", binary_img)
     cv.imshow("Binary output image", binary_img)
