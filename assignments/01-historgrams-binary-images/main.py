0e4a2fbf9cba01d1cb438b606f7d33f4a8b14421
Daeyoung Kim <dkim@cellinobio.com>
2024-09-08 14:01:44 -0400

    Clean up binary segmentation code

--- assignments/01-historgrams-binary-images/main.py
+++ assignments/01-historgrams-binary-images/main.py
@@ -1,12 +1,19 @@
 import cv2 as cv
-from os.path import expanduser
 from matplotlib import pyplot as plt
 
-home = expanduser('~')
-img_path = f'{home}/Downloads/IMAGE006.jpg' #Change with your path
-img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
-#plt.hist(img.ravel(),256,[0,256]); plt.show()
 
-threshold = 100
-binary_img = cv.threshold(img, threshold, 255, cv.THRESH_BINARY)[1]
-cv.imwrite('binary_img.png', binary_img)
+def main() -> None:
+    img_path = "assets/"
+    image_name = "IMAGE000.jpg"
+    img = cv.imread(img_path + image_name, cv.IMREAD_GRAYSCALE)
+
+    plt.hist(img.ravel(), 256, (0, 256))
+    plt.show()
+
+    threshold = 100
+    binary_img = cv.threshold(img, threshold, 255, cv.THRESH_BINARY)[1]
+    cv.imwrite(f"results/{image_name.split(".")[0]}_binary.png", binary_img)
+
+
+if __name__ == "__main__":
+    main()
