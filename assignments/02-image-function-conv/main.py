ca8ce2676489434879782d46b58ebb6d675a0fec
Dexter Friis-Hecht <dfriishecht@olin.edu>
2024-09-17 10:38:44 -0400

    Add template for assignment 2

---
+++ assignments/02-image-function-conv/main.py
@@ -0,0 +1,42 @@
+# Main working space for assignment 2.
+
+def change_brightness(image, amount):
+    # Add code to change brightness here
+    pass
+
+def change_contrast(image, amount):
+    # Add code to change contrast here
+    pass
+
+def change_brightness_alt(image, amount):
+    # Add code to change brightness of alternating pixels here
+    pass
+
+def change_colorspace(image, space):
+    # Add code to change colorspace here
+    pass
+
+def conv_identity(image, kernel_size):
+    # Add code to convolve image with identity kernel here
+    pass
+
+def conv_box(image, kernel_size):
+    # Add code to convolve image with box filter here
+    pass
+
+def conv_sharpen(image, kernel_size):
+    # Add code to convolve image with sharpen filter here
+    pass
+
+def conv_gaussian(image, kernel_size):
+    # Add code to convolve image with gaussian filter here
+    pass
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
